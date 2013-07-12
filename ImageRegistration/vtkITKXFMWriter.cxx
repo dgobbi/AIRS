fab78e4c07f9f66682b6e1ea695fac2e175eae3a
David Gobbi <david.gobbi@gmail.com>
2013-07-12 12:27:57 -0600

    Add a vtkITKXFMWriter to write ITK transforms.

---
+++ ImageRegistration/vtkITKXFMWriter.cxx
@@ -0,0 +1,320 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkITKXFMWriter.cxx
+
+Copyright (c) 2006 Atamai, Inc.
+
+Use, modification and redistribution of the software, in source or
+binary forms, are permitted provided that the following terms and
+conditions are met:
+
+1) Redistribution of the source code, in verbatim or modified
+   form, must retain the above copyright notice, this license,
+   the following disclaimer, and any notices that refer to this
+   license and/or the following disclaimer.
+
+2) Redistribution in binary form must include the above copyright
+   notice, a copy of this license and the following disclaimer
+   in the documentation or with other materials provided with the
+   distribution.
+
+3) Modified copies of the source code must be clearly marked as such,
+   and must not be misrepresented as verbatim copies of the source code.
+
+THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE SOFTWARE "AS IS"
+WITHOUT EXPRESSED OR IMPLIED WARRANTY INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE.  IN NO EVENT SHALL ANY COPYRIGHT HOLDER OR OTHER PARTY WHO MAY
+MODIFY AND/OR REDISTRIBUTE THE SOFTWARE UNDER THE TERMS OF THIS LICENSE
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, LOSS OF DATA OR DATA BECOMING INACCURATE
+OR LOSS OF PROFIT OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF
+THE USE OR INABILITY TO USE THE SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGES.
+
+=========================================================================*/
+
+#include "vtkITKXFMWriter.h"
+
+#include "vtkObjectFactory.h"
+
+#include "vtkMath.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+#include "vtkCollection.h"
+#include "vtkTransform.h"
+#include "vtkHomogeneousTransform.h"
+#include "vtkGeneralTransform.h"
+#include "vtkDoubleArray.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkPoints.h"
+
+#include <ctype.h>
+#include <stdio.h>
+
+#if !defined(_WIN32) || defined(__CYGWIN__)
+# include <unistd.h> /* unlink */
+#else
+# include <io.h> /* unlink */
+#endif
+
+#include <stack>
+
+//--------------------------------------------------------------------------
+vtkStandardNewMacro(vtkITKXFMWriter);
+
+//-------------------------------------------------------------------------
+vtkITKXFMWriter::vtkITKXFMWriter()
+{
+  this->FileName = 0;
+  this->Transform = 0;
+  this->Transforms = vtkCollection::New();
+}
+
+//-------------------------------------------------------------------------
+vtkITKXFMWriter::~vtkITKXFMWriter()
+{
+  if (this->Transforms)
+    {
+    this->Transforms->Delete();
+    }
+  if (this->Transform)
+    {
+    this->Transform->Delete();
+    }
+  if (this->FileName)
+    {
+    delete [] this->FileName;
+    }
+}
+
+//-------------------------------------------------------------------------
+void vtkITKXFMWriter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+
+  os << indent << "FileName: "
+     << (this->FileName ? this->FileName : "none") << "\n";
+  os << indent << "Transform: " << this->Transform << "\n";
+  if (this->Transform)
+    {
+    this->Transform->PrintSelf(os, indent.GetNextIndent());
+    }
+  os << indent << "NumberOfTransforms: "
+     << this->Transforms->GetNumberOfItems() << "\n";
+}
+
+//-------------------------------------------------------------------------
+int vtkITKXFMWriter::WriteLinearTransform(
+  ostream &outfile, vtkHomogeneousTransform *transform)
+{
+  vtkMatrix4x4 *matrix = transform->GetMatrix();
+
+  if (matrix->GetElement(3,0) != 0.0 ||
+      matrix->GetElement(3,1) != 0.0 ||
+      matrix->GetElement(3,2) != 0.0 ||
+      matrix->GetElement(3,3) != 1.0)
+    {
+    vtkErrorMacro("WriteLinearTransform: The transform is not linear");
+    return 0;
+    }
+
+  outfile << "Transform: MatrixOffsetTransformBase_double_3_3\n";
+
+  outfile << "Parameters:";
+
+  for (int i = 0; i < 3; i++)
+    {
+    outfile << " " << matrix->GetElement(i, 0)
+            << " " << matrix->GetElement(i, 1)
+            << " " << matrix->GetElement(i, 2);
+    }
+
+  outfile << " " << matrix->GetElement(0, 3)
+          << " " << matrix->GetElement(1, 3)
+          << " " << matrix->GetElement(2, 3);
+
+  outfile << "\n";
+
+  outfile << "FixedParameters:";
+  outfile << " " << 0.0 << " " << 0.0 << " " << 0.0;
+
+  outfile << "\n";
+
+  return 1;
+}
+
+//-------------------------------------------------------------------------
+int vtkITKXFMWriter::WriteTransform(
+  ostream &outfile, vtkAbstractTransform *transform)
+{
+  if (transform->IsA("vtkHomogeneousTransform"))
+    {
+    return this->WriteLinearTransform(
+      outfile, (vtkHomogeneousTransform *)transform);
+    }
+
+  vtkErrorMacro("Unsupported transform type "
+                << transform->GetClassName());
+
+  return 0;
+}
+
+//-------------------------------------------------------------------------
+int vtkITKXFMWriter::WriteFile()
+{
+  // Check that a transform has been set.
+  if (!this->Transform)
+    {
+    vtkErrorMacro("WriteFile: No input transform has been set.");
+    return 0;
+    }
+  // Check that the file name has been set.
+  if (!this->FileName)
+    {
+    vtkErrorMacro("WriteFile: No file name has been set.");
+    return 0;
+    }
+
+  // Open the file.
+  ofstream outfile(this->FileName, ios::out);
+
+  if (outfile.fail())
+    {
+    vtkErrorMacro("WriteFile: Can't create the file " << this->FileName);
+    return 0;
+    }
+
+  // Write the header
+  outfile << "#Insight Transform File V1.0\n";
+
+  // Push the transforms onto the stack in reverse order
+  std::stack<vtkAbstractTransform *> tstack;
+  int i = this->Transforms->GetNumberOfItems();
+  while (i > 0)
+    {
+    tstack.push(
+      ((vtkAbstractTransform *)this->Transforms->GetItemAsObject(--i)));
+    }
+  tstack.push(this->Transform);
+
+  // Write out all the transforms on the stack
+  int status = 1;
+  int count = 0;
+  while (status != 0 && !tstack.empty())
+    {
+    vtkAbstractTransform *transform = tstack.top();
+    tstack.pop();
+
+    if (transform->IsA("vtkGeneralTransform"))
+      {
+      // Decompose general transforms
+      vtkGeneralTransform *gtrans = (vtkGeneralTransform *)transform;
+      int n = gtrans->GetNumberOfConcatenatedTransforms();
+      while (n > 0)
+        {
+        tstack.push(gtrans->GetConcatenatedTransform(--n));
+        }
+      }
+    else
+      {
+      // Write all other kinds of transforms
+      outfile << "#Transform " << count << "\n";
+      status = this->WriteTransform(outfile, transform);
+      count++;
+      }
+    }
+
+  outfile.close();
+
+  if (status == 0)
+    {
+    unlink(this->FileName);
+    }
+
+  return status;
+}
+
+//-------------------------------------------------------------------------
+int vtkITKXFMWriter::ProcessRequest(vtkInformation *request,
+                                    vtkInformationVector **inputVector,
+                                    vtkInformationVector *outputVector)
+{
+  if (request->Has(vtkDemandDrivenPipeline::REQUEST_DATA()))
+    {
+    if (this->Transform)
+      {
+      this->Transform->Update();
+      }
+    int n = this->Transforms->GetNumberOfItems();
+    for (int i = 0; i < n; i++)
+      {
+      ((vtkAbstractTransform *)this->Transforms->GetItemAsObject(i))
+        ->Update();
+      }
+    return this->WriteFile();
+    }
+
+  return this->Superclass::ProcessRequest(request, inputVector, outputVector);
+}
+
+//-------------------------------------------------------------------------
+void vtkITKXFMWriter::Write()
+{
+  this->Modified();
+  this->Update();
+}
+
+//-------------------------------------------------------------------------
+int vtkITKXFMWriter::GetNumberOfTransforms()
+{
+  if (this->Transform == 0)
+    {
+    return 0;
+    }
+
+  return (1 + this->Transforms->GetNumberOfItems());
+}
+
+//-------------------------------------------------------------------------
+void vtkITKXFMWriter::SetTransform(vtkAbstractTransform *transform)
+{
+  if (transform == this->Transform)
+    {
+    return;
+    }
+
+  if (this->Transform != 0)
+    {
+    this->Transform->Delete();
+    }
+
+  if (transform != 0)
+    {
+    transform->Register(this);
+    }
+
+  this->Transform = transform;
+  this->Transforms->RemoveAllItems();
+  this->Modified();
+}
+
+//-------------------------------------------------------------------------
+void vtkITKXFMWriter::AddTransform(vtkAbstractTransform *transform)
+{
+  if (transform == 0)
+    {
+    return;
+    }
+
+  if (this->Transform == 0)
+    {
+    this->SetTransform(transform);
+    }
+  else
+    {
+    this->Transforms->AddItem(transform);
+    this->Modified();
+    }
+}
---
+++ ImageRegistration/vtkITKXFMWriter.h
@@ -0,0 +1,128 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkITKXFMWriter.h
+
+Copyright (c) 2006 Atamai, Inc.
+
+Use, modification and redistribution of the software, in source or
+binary forms, are permitted provided that the following terms and
+conditions are met:
+
+1) Redistribution of the source code, in verbatim or modified
+   form, must retain the above copyright notice, this license,
+   the following disclaimer, and any notices that refer to this
+   license and/or the following disclaimer.
+
+2) Redistribution in binary form must include the above copyright
+   notice, a copy of this license and the following disclaimer
+   in the documentation or with other materials provided with the
+   distribution.
+
+3) Modified copies of the source code must be clearly marked as such,
+   and must not be misrepresented as verbatim copies of the source code.
+
+THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE SOFTWARE "AS IS"
+WITHOUT EXPRESSED OR IMPLIED WARRANTY INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE.  IN NO EVENT SHALL ANY COPYRIGHT HOLDER OR OTHER PARTY WHO MAY
+MODIFY AND/OR REDISTRIBUTE THE SOFTWARE UNDER THE TERMS OF THIS LICENSE
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, LOSS OF DATA OR DATA BECOMING INACCURATE
+OR LOSS OF PROFIT OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF
+THE USE OR INABILITY TO USE THE SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGES.
+
+=========================================================================*/
+// .NAME vtkITKXFMWriter - A writer for ITK transformation files.
+// .SECTION Description
+// ITK uses a text file to save its transformations.  The file contains
+// the name of the transform class and the parameters.  Not all ITK
+// transform types are supported by this reader, only linear transforms
+// that can be represented by a 4x4 matrix.
+// .SECTION See Also
+// vtkITKTransformReader vtkMNITransformReader
+// .SECTION Thanks
+// Thanks to David Gobbi for writing this class.
+
+#ifndef __vtkITKXFMWriter_h
+#define __vtkITKXFMWriter_h
+
+#include "vtkAlgorithm.h"
+
+class vtkAbstractTransform;
+class vtkHomogeneousTransform;
+class vtkThinPlateSplineTransform;
+class vtkGridTransform;
+class vtkCollection;
+
+class VTK_HYBRID_EXPORT vtkITKXFMWriter : public vtkAlgorithm
+{
+public:
+  vtkTypeMacro(vtkITKXFMWriter,vtkAlgorithm);
+
+  static vtkITKXFMWriter *New();
+  virtual void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Set the file name.
+  vtkSetStringMacro(FileName);
+  vtkGetStringMacro(FileName);
+
+  // Description:
+  // Get the entension for this file format.
+  virtual const char* GetFileExtensions() {
+    return ".txt" ".tfm"; }
+
+  // Description:
+  // Get the name of this file format.
+  virtual const char* GetDescriptiveName() {
+    return "ITK Transform"; }
+
+  // Description:
+  // Set the transform.
+  virtual void SetTransform(vtkAbstractTransform *transform);
+  virtual vtkAbstractTransform *GetTransform() {
+    return this->Transform; };
+
+  // Description:
+  // Add another transform to the file.  The next time that
+  // SetTransform is called, all added transforms will be
+  // removed.
+  virtual void AddTransform(vtkAbstractTransform *transform);
+
+  // Description:
+  // Get the number of transforms that will be written.
+  virtual int GetNumberOfTransforms();
+
+  // Description:
+  // Write the file.
+  virtual void Write();
+
+protected:
+  vtkITKXFMWriter();
+  ~vtkITKXFMWriter();
+
+  char *FileName;
+  vtkAbstractTransform *Transform;
+  vtkCollection *Transforms;
+
+  int WriteLinearTransform(ostream &outfile,
+                           vtkHomogeneousTransform *transform);
+
+  virtual int WriteTransform(ostream &outfile,
+                             vtkAbstractTransform *transform);
+
+  virtual int WriteFile();
+
+  virtual int ProcessRequest(vtkInformation* request,
+                             vtkInformationVector** inInfo,
+                             vtkInformationVector* outInfo);
+
+private:
+  vtkITKXFMWriter(const vtkITKXFMWriter&); // Not implemented
+  void operator=(const vtkITKXFMWriter&);  // Not implemented
+
+};
+
+#endif
