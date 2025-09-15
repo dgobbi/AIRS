c113c155fe267998ff8db0e2f605d1d3ed483987
David Gobbi <david.gobbi@gmail.com>
2025-09-14 22:25:51 -0600

    Update CMake scripts to work with VTK 7.1 and later

--- CMake/AIRSCommonBlock.cmake
+++ CMake/AIRSCommonBlock.cmake
@@ -1,154 +1,117 @@
-# Need to include class headers and the configuration header.
-INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR})
+# Handle common tasks for VTK < 9.0
 
-INCLUDE_DIRECTORIES(${AIRS_INCLUDE_DIRS})
+# Automatically find the header for each cxx file
+foreach(arg ${LIB_SRCS})
+  get_filename_component(src "${arg}" ABSOLUTE)
+  string(REGEX REPLACE "\\.(cxx|c|mm|m)$" ".h" hdr "${src}")
+  if("${hdr}" MATCHES "\\.h$" AND EXISTS "${hdr}")
+    list(APPEND LIB_HDRS "${hdr}")
+  endif()
+endforeach()
 
-# Add the wrapper libraries
-SET(KIT_TCL_LIBS "")
-SET(KIT_PYTHON_LIBS "")
-SET(KIT_JAVA_LIBS "")
+# Create the hierarchy file
+if(BUILD_PYTHON_WRAPPERS)
+  set_source_files_properties(${LIB_SPECIAL} PROPERTIES WRAP_SPECIAL ON)
+  set(MODULE_HIERARCHY_NAME ${LIB_NAME}Hierarchy)
+  # _LINK_DEPENDS is a variable suffix from the VTK module macros
+  set(${LIB_NAME}_LINK_DEPENDS ${VTK_LIBS})
+  include(vtkWrapHierarchy)
+  vtk_wrap_hierarchy(${LIB_NAME} ${CMAKE_CURRENT_BINARY_DIR} "${LIB_SRCS}")
+  set(KIT_HIERARCHY_FILE ${CMAKE_CURRENT_BINARY_DIR}/${MODULE_HIERARCHY_NAME}.txt)
+  set(LIB_HIERARCHY_STAMP ${CMAKE_CURRENT_BINARY_DIR}/${MODULE_HIERARCHY_NAME}.stamp.txt)
+endif()
 
-# Add the kit libraries to the wrapper libraries
-FOREACH( lib ${KIT_LIBS})
-  SET(KIT_TCL_LIBS ${KIT_TCL_LIBS} "${lib}TCL")
+# Set the library name suffix for VTK libraries
+set(LIB_NAME_SUFFIX "-${VTK_MAJOR_VERSION}.${VTK_MINOR_VERSION}")
+if(DEFINED VTK_CUSTOM_LIBRARY_SUFFIX)
+  set(LIB_NAME_SUFFIX "${VTK_CUSTOM_LIBRARY_SUFFIX}")
+endif()
 
-  IF(APPLE OR ${VTK_MAJOR_VERSION} GREATER 4)
-    SET(KIT_PYTHON_LIBS ${KIT_PYTHON_LIBS} "${lib}PythonD")
-  ELSE(APPLE OR ${VTK_MAJOR_VERSION} GREATER 4)
-    SET(KIT_PYTHON_LIBS ${KIT_PYTHON_LIBS} "${lib}Python")
-  ENDIF(APPLE OR ${VTK_MAJOR_VERSION} GREATER 4)
+# Create the library
+add_library(${LIB_NAME} ${LIB_SRCS} ${LIB_HIERARCHY_STAMP})
+set_target_properties(${LIB_NAME} PROPERTIES
+  OUTPUT_NAME ${LIB_NAME}${LIB_NAME_SUFFIX})
+set_target_properties(${LIB_NAME} PROPERTIES
+  VERSION "${${PROJECT_NAME}_VERSION}"
+  SOVERSION "${${PROJECT_NAME}_MAJOR_VERSION}.${${PROJECT_NAME}_MINOR_VERSION}")
+if(BUILD_PYTHON_WRAPPERS)
+  set_target_properties(${LIB_NAME} PROPERTIES POSITION_INDEPENDENT_CODE ON)
+endif()
+target_link_libraries(${LIB_NAME} LINK_PUBLIC ${VTK_LIBS})
 
-  SET(KIT_JAVA_LIBS ${KIT_JAVA_LIBS} "${lib}Java")
-ENDFOREACH( lib ${KIT_LIBS})
+# Wrappers
+if(BUILD_PYTHON_WRAPPERS)
+  set(MODULE_PYTHON_NAME ${LIB_NAME}Python)
+  set(${LIB_NAME}_WRAP_DEPENDS ${VTK_LIBS})
+  include(vtkWrapPython)
+  include_directories(${vtkPython_INCLUDE_DIRS})
+  set(VTK_PYTHON_LIBRARIES ${vtkPython_LIBRARIES})
+  set(LIB_PYTHON_LIBS vtkWrappingPythonCore)
+  vtk_wrap_python3(${MODULE_PYTHON_NAME} LIB_PYTHON_SRCS "${LIB_SRCS}")
+  if(AIRS_PYTHON_LIBRARIES)
+    # do things the old way, with PythonD libraries
+    set(XY) # Get python version, e.g. 38 for python 3.8
+    if(vtkPython_LIBRARIES)
+      list(GET vtkPython_LIBRARIES 0 TMP_LIB_NAME)
+      get_filename_component(TMP_NAME "${TMP_LIB_NAME}" NAME)
+      string(REGEX REPLACE "^[^0-9]*([0-9])\\.*([0-9]).*$" "\\1\\2"
+        XY "${TMP_NAME}")
+      if(NOT XY)
+        set(XY)
+      endif()
+    endif()
+    set(LIB_PYTHON_NAME ${LIB_NAME}PythonD)
+    set(LIB_PYTHON_OUTPUT_NAME ${LIB_NAME}Python${XY}D)
+    set(LIB_PYTHON_OUTPUT_NAME ${LIB_PYTHON_OUTPUT_NAME}${LIB_NAME_SUFFIX})
+    foreach(TMP_LIB ${VTK_LIBS})
+      set(LIB_PYTHON_LIBS ${LIB_PYTHON_LIBS} ${TMP_LIB}PythonD)
+    endforeach()
+    add_library(${LIB_PYTHON_NAME} ${LIB_PYTHON_SRCS} ${LIB_PYTHON_EXTRA_SRCS})
+    set_target_properties(${LIB_PYTHON_NAME} PROPERTIES
+      POSITION_INDEPENDENT_CODE ON)
+    set_target_properties(${LIB_PYTHON_NAME} PROPERTIES
+      VERSION "${PROJECT_VERSION}"
+      SOVERSION "${PROJECT_MAJOR_VERSION}.${PROJECT_MINOR_VERSION}"
+      OUTPUT_NAME "${LIB_PYTHON_OUTPUT_NAME}")
+    target_link_libraries(${LIB_PYTHON_NAME} LINK_PUBLIC
+      ${LIB_NAME} ${LIB_PYTHON_LIBS})
+    # On Win32 and Mac, link python library non-private
+    if(WIN32 OR APPLE)
+      target_link_libraries(${LIB_PYTHON_NAME} LINK_PUBLIC
+        ${VTK_PYTHON_LIBRARIES})
+    else()
+      target_link_libraries(${LIB_PYTHON_NAME} LINK_PRIVATE
+        ${VTK_PYTHON_LIBRARIES})
+    endif()
+    if(KIT_PYTHON_DEPS)
+      add_dependencies(${LIB_PYTHON_NAME} ${KIT_PYTHON_DEPS})
+    endif()
+    add_library(${MODULE_PYTHON_NAME} MODULE ${MODULE_PYTHON_NAME}Init.cxx)
+    target_link_libraries(${MODULE_PYTHON_NAME} ${LIB_PYTHON_NAME})
+  else()
+    # do things the new way, without PythonD libraries
+    add_library(${MODULE_PYTHON_NAME} MODULE ${MODULE_PYTHON_NAME}Init.cxx
+      ${LIB_PYTHON_SRCS} ${LIB_PYTHON_EXTRA_SRCS})
+    target_link_libraries(${MODULE_PYTHON_NAME} LINK_PRIVATE
+      ${LIB_NAME} ${LIB_PYTHON_LIBS} ${VTK_PYTHON_LIBRARIES})
+    if(KIT_PYTHON_DEPS)
+      add_dependencies(${MODULE_PYTHON_NAME} ${KIT_PYTHON_DEPS})
+    endif()
+  endif()
+  set_target_properties(${MODULE_PYTHON_NAME} PROPERTIES PREFIX "")
+  if(WIN32 AND NOT CYGWIN)
+    set_target_properties(${MODULE_PYTHON_NAME} PROPERTIES SUFFIX ".pyd")
+  endif()
+  set_target_properties(${MODULE_PYTHON_NAME} PROPERTIES NO_SONAME 1)
+endif()
 
-## Setup vtkInstantiator registration for this library's classes.
-#VTK_MAKE_INSTANTIATOR3(vtk${KIT}Instantiator KitInstantiator_SRCS
-#                       ${Kit_SRCS}
-#                       VTK_${UKIT}_EXPORT
-#                       ${VTK_BINARY_DIR} "")
+# Set the install rules for the library
+install(TARGETS
+  ${LIB_NAME} ${LIB_PYTHON_NAME} ${MODULE_PYTHON_NAME}
+  EXPORT AIRSTargets
+  RUNTIME DESTINATION ${AIRS_RUNTIME_INSTALL_DEST} COMPONENT RuntimeLibraries
+  LIBRARY DESTINATION ${AIRS_LIBRARY_INSTALL_DEST} COMPONENT RuntimeLibraries
+  ARCHIVE DESTINATION ${AIRS_ARCHIVE_INSTALL_DEST} COMPONENT Development)
 
-#ADD_LIBRARY(vtk${KIT} ${Kit_SRCS} ${Kit_EXTRA_SRCS} ${KitInstantiator_SRCS})
-ADD_LIBRARY(vtk${KIT} ${Kit_SRCS} ${Kit_EXTRA_SRCS})
-
-# Do the TCL wrapping
-#
-IF(VTK_WRAP_TCL)
-  IF(VTK_MAJOR_VERSION GREATER 4)
-    # First, the VTK 5 style wrapping
-    #
-    VTK_WRAP_TCL3(vtk${KIT}TCL KitTCL_SRCS
-      "${Kit_SRCS}"
-      "${Kit_TCL_EXTRA_CMDS}")
-    ADD_LIBRARY(vtk${KIT}TCL ${KitTCL_SRCS} ${Kit_TCL_EXTRA_SRCS})
-    SET(KIT_LIBRARY_TARGETS ${KIT_LIBRARY_TARGETS} vtk${KIT}TCL)
-    TARGET_LINK_LIBRARIES (vtk${KIT}TCL vtk${KIT} ${KIT_TCL_LIBS})
-    IF(KIT_TCL_DEPS)
-      ADD_DEPENDENCIES(vtk${KIT}TCL ${KIT_TCL_DEPS})
-    ENDIF(KIT_TCL_DEPS)
-  ELSE(VTK_MAJOR_VERSION GREATER 4)
-    # Do the VTK 4 style wrapping
-    #
-    VTK_WRAP_TCL2(vtk${KIT}TCL SOURCES KitTCL_SRCS
-      "${Kit_SRCS}"
-      COMMANDS "${Kit_TCL_EXTRA_CMDS}")
-    ADD_LIBRARY(vtk${KIT}TCL ${KitTCL_SRCS} ${Kit_TCL_EXTRA_SRCS})
-    TARGET_LINK_LIBRARIES (vtk${KIT}TCL vtk${KIT} ${KIT_TCL_LIBS})
-    INSTALL_TARGETS(/lib/vtk vtk${KIT}TCL)
-    SUBDIR_DEPENDS(Wrapping/Tcl vtk${KIT})
-  ENDIF(VTK_MAJOR_VERSION GREATER 4)
-ENDIF(VTK_WRAP_TCL)
-
-# Do the Python wrapping
-#
-IF(VTK_WRAP_PYTHON)
-  IF(VTK_MAJOR_VERSION GREATER 4)
-    # First, the VTK 5 style wrapping
-    #
-    VTK_WRAP_PYTHON3(vtk${KIT}Python KitPython_SRCS "${Kit_SRCS}")
-    ADD_LIBRARY(vtk${KIT}PythonD ${KitPython_SRCS} ${Kit_PYTHON_EXTRA_SRCS})
-    TARGET_LINK_LIBRARIES(vtk${KIT}PythonD vtk${KIT} ${KIT_PYTHON_LIBS})
-    SET(KIT_LIBRARY_TARGETS ${KIT_LIBRARY_TARGETS} vtk${KIT}PythonD)
-    # On Win32 and Mac, link python library now, on UNIX, don't
-    IF(WIN32 OR APPLE)
-      TARGET_LINK_LIBRARIES (vtk${KIT}PythonD ${VTK_PYTHON_LIBRARIES})
-    ENDIF(WIN32 OR APPLE)
-    IF(KIT_PYTHON_DEPS)
-      ADD_DEPENDENCIES(vtk${KIT}PythonD ${KIT_PYTHON_DEPS})
-    ENDIF(KIT_PYTHON_DEPS)
-    ADD_LIBRARY(vtk${KIT}Python MODULE vtk${KIT}PythonInit.cxx)
-    SET_TARGET_PROPERTIES(vtk${KIT}Python PROPERTIES PREFIX "")
-    IF(WIN32 AND NOT CYGWIN)
-      SET_TARGET_PROPERTIES(vtk${KIT}Python PROPERTIES SUFFIX ".pyd")
-    ENDIF(WIN32 AND NOT CYGWIN)
-
-    TARGET_LINK_LIBRARIES(vtk${KIT}Python vtk${KIT}PythonD)
-    # On Win32 and Mac, link python library now, on UNIX, don't
-    IF(WIN32 OR APPLE)
-      TARGET_LINK_LIBRARIES (vtk${KIT}Python ${VTK_PYTHON_LIBRARIES})
-    ENDIF(WIN32 OR APPLE)
-  ELSE(VTK_MAJOR_VERSION GREATER 4)
-    # Do the VTK 4 style wrapping
-    #
-    VTK_WRAP_PYTHON2(vtk${KIT}Python KitPython_SRCS ${Kit_SRCS})
-    IF (APPLE)
-      ADD_LIBRARY(vtk${KIT}PythonD ${KitPython_SRCS} ${Kit_PYTHON_EXTRA_SRCS})
-      ADD_LIBRARY(vtk${KIT}Python MODULE vtk${KIT}PythonInit.cxx)
-      TARGET_LINK_LIBRARIES(vtk${KIT}PythonD vtk${KIT} ${KIT_PYTHON_LIBS}
-        ${PYTHON_LIBRARY})
-      TARGET_LINK_LIBRARIES(vtk${KIT}Python vtk${KIT}PythonD)
-      INSTALL_TARGETS(/lib/vtk vtk${KIT}PythonD)
-    ELSE (APPLE)
-      ADD_LIBRARY(vtk${KIT}Python MODULE ${KitPython_SRCS}
-        ${Kit_PYTHON_EXTRA_SRCS})
-      TARGET_LINK_LIBRARIES (vtk${KIT}Python vtk${KIT} ${KIT_PYTHON_LIBS})
-    ENDIF(APPLE)
-    IF(WIN32)
-      TARGET_LINK_LIBRARIES (vtk${KIT}Python
-        debug ${PYTHON_DEBUG_LIBRARY}
-        optimized ${PYTHON_LIBRARY})
-    ENDIF(WIN32)
-    IF(WIN32 AND NOT CYGWIN)
-      SET_TARGET_PROPERTIES(vtk${KIT}Python PROPERTIES SUFFIX ".pyd")
-    ENDIF(WIN32 AND NOT CYGWIN)
-    INSTALL_TARGETS(/lib/vtk vtk${KIT}Python)
-  ENDIF(VTK_MAJOR_VERSION GREATER 4)
-ENDIF (VTK_WRAP_PYTHON)
-
-# Do the Java wrapping
-#
-IF (VTK_WRAP_JAVA)
-  IF(VTK_MAJOR_VERSION GREATER 4)
-    # Do the VTK 5 style wrapping
-    #
-    VTK_WRAP_JAVA3(vtk${KIT}Java KitJava_SRCS "${Kit_SRCS}")
-    ADD_LIBRARY(vtk${KIT}Java SHARED ${KitJava_SRCS} ${Kit_JAVA_EXTRA_SRCS})
-    SET(KIT_LIBRARY_TARGETS ${KIT_LIBRARY_TARGETS} vtk${KIT}Java)
-    TARGET_LINK_LIBRARIES(vtk${KIT}Java vtk${KIT} ${KIT_JAVA_LIBS})
-    IF(KIT_JAVA_DEPS)
-      ADD_DEPENDENCIES(vtk${KIT}Java ${KIT_JAVA_DEPS})
-    ENDIF(KIT_JAVA_DEPS)
-  ELSE(VTK_MAJOR_VERSION GREATER 4)
-    # Do the VTK 4 style wrapping
-    #
-    VTK_WRAP_JAVA2(vtk${KIT}Java KitJava_SRCS ${Kit_SRCS})
-    ADD_LIBRARY(vtk${KIT}Java SHARED ${KitJava_SRCS} ${Kit_JAVA_EXTRA_SRCS})
-    TARGET_LINK_LIBRARIES(vtk${KIT}Java vtk${KIT} ${KIT_JAVA_LIBS})
-    INSTALL_TARGETS(/lib/vtk vtk${KIT}Java)
-  ENDIF(VTK_MAJOR_VERSION GREATER 4)
-ENDIF (VTK_WRAP_JAVA)
-
-TARGET_LINK_LIBRARIES(vtk${KIT} ${KIT_LIBS} ${KIT_EXTRA_LIBS})
-
-INSTALL_TARGETS(/lib/vtk vtk${KIT})
-INSTALL_FILES(/include/vtk .h ${Kit_SRCS})
-
-# If the user defined a custom macro, execute it now and pass in all the srcs
-#
-IF(LOCALUSERMACRODEFINED)
-  LocalUserOptionsMacro( "${Kit_SRCS}"       "${Kit_EXTRA_SRCS}"
-                         "${KitTCL_SRCS}"    "${Kit_TCL_EXTRA_SRCS}"
-                         "${KitJava_SRCS}"   "${Kit_JAVA_EXTRA_SRCS}"
-                         "${KitPython_SRCS}" "${Kit_PYTHON_EXTRA_SRCS}")
-ENDIF(LOCALUSERMACRODEFINED)
-
-# End of common section
+install(FILES ${LIB_HDRS}
+  DESTINATION ${AIRS_INCLUDE_INSTALL_DEST} COMPONENT Development)
